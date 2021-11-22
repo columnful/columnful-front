0b6ffef1b6b02d211960ba3928e51012ad4b7829
rosieyeon <sseyeon956@gmail.com>
2021-11-22 23:21:23 +0900

    feat: movie-home

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,9 +1,42 @@
 <template>
-  
+  <div class="card">
+    <router-link :to="``">
+      <img 
+        class="card_image" 
+        :src="'https://image.tmdb.org/t/p/'+movie.poster_path">
+      
+      <div class="card_content">
+        <p class="card_content_title">
+          {{movie.title}}
+        </p>
+      </div>
+
+    </router-link>
+
+  </div>
+
 </template>
 
 <script>
+// import { Carousel, Slide } from "vue-carousel"
+// import axios from "axios"
+
+// const URL_PREFIX = 'https://image.tmdb.org/t/p/w500'
+
 export default {
+  name:"MovieCard",
+  components: {
+    // Carousel,
+    // Slide,
+  },
+  props: {
+    movie: {
+      type: Array
+    },
+    isLogin: {
+      type: Boolean
+    }
+  },
 
 }
 </script>
--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,46 +1,60 @@
 <template>
   <div>
-    <h1>Movie</h1>
-      <div id="movie_cards">
+      <carousel v-model=index :per-page="1" class="banner_list"
+        paginationActiveColor="#e01a31"
+        paginationColor="#999"
+        :paginationPadding=3>
+        <slide class="slide_item">
+          <img class="banner_img" src="../images/banner/about_time_cropped.jpg" style="width:100%">
+        </slide>
+      </carousel>
+      
+      <div id="movie_card">
 
-      <Movies
-        :movies="toprated_movies"
-        @click.native="newAlert"
-        group_title="TOP RANKED"
-        class="mt-5"
-      />
-
-      <Movies
+      <h2>POPULAR</h2>
+      <MovieCard
         :movies="popular_movies"
         @click.native="newAlert"
         group_title="POPULAR"
         class="mt-5"
       />
 
-      <Movies
+      <h2>TOP RATED</h2>
+      <MovieCard
+        :movies="toprated_movies"
+        @click.native="newAlert"
+        group_title="TOP RANKED"
+        class="mt-5"
+      />
+
+      <!-- <MovieCard
         :isLogin="isLogin"
-        :movies="nowplaying_movies"
+        :movies="recommend_movie_user"
         @click.native="newAlert"
         group_title="RECOMMENDATION"
         class="mt-5"
-      />
+      /> -->
 
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import { Carousel, Slide } from "vue-carousel"
 import MovieCard from '@/components/MovieCard'
+import axios from 'axios'
+
 const MOVIE_DB_API_URL_POPULAR = 'https://api.themoviedb.org/3/movie/popular'
 const MOVIE_DB_API_URL_TOP_RATED = 'https://api.themoviedb.org/3/movie/top_rated'
 const MOVIE_DB_API_KEY = 'd96fad9400bff08e7653e874066ac99b'
+
 export default {
   name: 'Movie',
   data: function () {
     return {
       popular_movies: [],
       toprated_movies: [],
+      recommend_movie_user: [],
     }
   },
   props: {
@@ -50,6 +64,8 @@ export default {
   },
   components: {
     MovieCard,
+    Carousel,
+    Slide
   },
   created: function () {
     axios.get(MOVIE_DB_API_URL_POPULAR, {
@@ -76,6 +92,21 @@ export default {
       this.toprated_movies = res.data.results
     })
     .catch(err => console.log(err))
+
+    const token = localStorage.getItem('jwt')
+    const config = {
+      headers: {
+        Authorization: `JWT ${token}`
+      }
+    }
+    axios.get('http://127.0.0.1:8000/movies/recommend_movie_user/', config)
+      .then((res) => {
+        console.log(res)
+        this.recommend_movie_user = res.data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   },
   methods: {
     newAlert: function () {
@@ -94,5 +125,8 @@ export default {
 </script>
 
 <style>
+  .banner_img {
+    opacity: 0.77;
+  }
 
 </style>
