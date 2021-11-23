5cd834fbc0d6db17e26d98097314c874c4d44ebd
rosieyeon <sseyeon956@gmail.com>
2021-11-23 17:53:10 +0900

    feat: movie-modal

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,92 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="app">
+    <button class="btn btn--primary mx-auto" @click="$refs.modalName.openModal()">Open modal</button>
+
+    <h1>Title page</h1>
+    <p>I need to make the page scrollable to show multiple functionalities of this modal component that's why I'll add a lot of lorem ipsum :D</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed velit dignissim sodales ut eu sem integer vitae. Id aliquet lectus proin nibh nisl condimentum. Fringilla urna porttitor rhoncus dolor purus. Nam aliquam sem et tortor. Nisl vel pretium lectus quam id. Cras pulvinar mattis nunc sed. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Tristique magna sit amet purus. Fermentum et sollicitudin ac orci phasellus egestas tellus. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Felis eget nunc lobortis mattis aliquam faucibus. Tincidunt eget nullam non nisi est sit amet facilisis. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut. Nec nam aliquam sem et tortor consequat id. Commodo nulla facilisi nullam vehicula ipsum a. Elementum tempus egestas sed sed. Faucibus purus in massa tempor nec feugiat nisl pretium fusce.</p>
+    <p>Arcu cursus vitae congue mauris rhoncus aenean. Tempor id eu nisl nunc mi. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Ut faucibus pulvinar elementum integer enim. Odio facilisis mauris sit amet massa vitae tortor condimentum lacinia. Eu non diam phasellus vestibulum lorem sed risus. Porttitor rhoncus dolor purus non enim praesent. Sit amet mauris commodo quis imperdiet. Lobortis feugiat vivamus at augue eget. Nibh tellus molestie nunc non blandit. Tellus mauris a diam maecenas sed enim ut. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Mattis aliquam faucibus purus in massa.</p>
+    <p>Enim nec dui nunc mattis. Nisi lacus sed viverra tellus in. Cursus in hac habitasse platea dictumst quisque sagittis. Morbi tincidunt augue interdum velit. Sed blandit libero volutpat sed cras. A erat nam at lectus urna. Donec massa sapien faucibus et molestie. Fames ac turpis egestas sed tempus. Mauris rhoncus aenean vel elit scelerisque mauris. Vivamus at augue eget arcu dictum. Tempus imperdiet nulla malesuada pellentesque elit eget gravida. Dignissim cras tincidunt lobortis feugiat vivamus at augue eget. Ut consequat semper viverra nam. Ipsum dolor sit amet consectetur adipiscing elit. Donec enim diam vulputate ut pharetra sit.</p>
+    <p>Arcu cursus vitae congue mauris rhoncus aenean. Tempor id eu nisl nunc mi. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Ut faucibus pulvinar elementum integer enim. Odio facilisis mauris sit amet massa vitae tortor condimentum lacinia. Eu non diam phasellus vestibulum lorem sed risus. Porttitor rhoncus dolor purus non enim praesent. Sit amet mauris commodo quis imperdiet. Lobortis feugiat vivamus at augue eget. Nibh tellus molestie nunc non blandit. Tellus mauris a diam maecenas sed enim ut. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Mattis aliquam faucibus purus in massa.</p>
+    <p>Arcu cursus vitae congue mauris rhoncus aenean. Tempor id eu nisl nunc mi. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Ut faucibus pulvinar elementum integer enim. Odio facilisis mauris sit amet massa vitae tortor condimentum lacinia. Eu non diam phasellus vestibulum lorem sed risus. Porttitor rhoncus dolor purus non enim praesent. Sit amet mauris commodo quis imperdiet. Lobortis feugiat vivamus at augue eget. Nibh tellus molestie nunc non blandit. Tellus mauris a diam maecenas sed enim ut. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Mattis aliquam faucibus purus in massa.</p>
+    <p>Enim nec dui nunc mattis. Nisi lacus sed viverra tellus in. Cursus in hac habitasse platea dictumst quisque sagittis. Morbi tincidunt augue interdum velit. Sed blandit libero volutpat sed cras. A erat nam at lectus urna. Donec massa sapien faucibus et molestie. Fames ac turpis egestas sed tempus. Mauris rhoncus aenean vel elit scelerisque mauris. Vivamus at augue eget arcu dictum. Tempus imperdiet nulla malesuada pellentesque elit eget gravida. Dignissim cras tincidunt lobortis feugiat vivamus at augue eget. Ut consequat semper viverra nam. Ipsum dolor sit amet consectetur adipiscing elit. Donec enim diam vulputate ut pharetra sit.</p>
+
+    <modal ref="modalName">
+      <template v-slot:header>
+        <h1>Modal title</h1>
+      </template>
+
+      <template v-slot:body>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed velit dignissim sodales ut eu sem integer vitae. Id aliquet lectus proin nibh nisl condimentum. Fringilla urna porttitor rhoncus dolor purus. Nam aliquam sem et tortor. Nisl vel pretium lectus quam id. Cras pulvinar mattis nunc sed. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Tristique magna sit amet purus. Fermentum et sollicitudin ac orci phasellus egestas tellus. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Felis eget nunc lobortis mattis aliquam faucibus. Tincidunt eget nullam non nisi est sit amet facilisis. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut. Nec nam aliquam sem et tortor consequat id. Commodo nulla facilisi nullam vehicula ipsum a. Elementum tempus egestas sed sed. Faucibus purus in massa tempor nec feugiat nisl pretium fusce.</p>
+        <p>Arcu cursus vitae congue mauris rhoncus aenean. Tempor id eu nisl nunc mi. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Ut faucibus pulvinar elementum integer enim. Odio facilisis mauris sit amet massa vitae tortor condimentum lacinia. Eu non diam phasellus vestibulum lorem sed risus. Porttitor rhoncus dolor purus non enim praesent. Sit amet mauris commodo quis imperdiet. Lobortis feugiat vivamus at augue eget. Nibh tellus molestie nunc non blandit. Tellus mauris a diam maecenas sed enim ut. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Mattis aliquam faucibus purus in massa.</p>
+      </template>
+
+      <template v-slot:footer>
+        <div class="d-flex align-items-center justify-content-between">
+          <button class="btn btn--secondary" @click="$refs.modalName.closeModal()">Cancel</button>
+          <button class="btn btn--primary" @click="$refs.modalName.closeModal()">Save</button>
+        </div>
+      </template>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from "@/components/Modal";
 
 export default {
-  name: 'About',
-
-}
+  name: "About",
+  components: {
+    Modal
+  }
+};
 </script>
+
+<style lang="scss">
+.btn {
+  padding: 8px 16px;
+  border-radius: 5px;
+  &--primary {
+    background-color: green;
+    color: #fff;
+  }
+  &--secondary {
+    background-color: #dddd;
+    color: #000;
+  }
+}
+
+// utilities
+.overflow-hidden {
+  overflow: hidden;
+}
+.mx-auto {
+  margin-left: auto;
+  margin-right: auto;
+}
+.d-flex {
+  display: flex;
+}
+.align-items-center {
+  align-items: center;
+}
+.justify-content-between {
+  justify-content: space-between;
+}
+// reset
+button {
+  background: none;
+  border: none;
+  outline: inherit;
+  cursor: pointer;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin: 0;
+}
+</style>
