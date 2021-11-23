4c92b2872762b414a6642532c0477ac1a6972571
rosieyeon <sseyeon956@gmail.com>
2021-11-23 15:27:37 +0900

    feat: movie-home

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,47 +1,60 @@
 <template>
   <div>
-    <h1>Movie</h1>
-      <movie-card></movie-card>
-      <div id="movie_cards">
+      <carousel v-model=index :per-page="1" class="banner_list"
+        paginationActiveColor="#e01a31"
+        paginationColor="#999"
+        :paginationPadding=3>
+        <slide class="slide_item">
+          <img class="banner_img" src="../images/banner/about_time_cropped.jpg" style="width:100%">
+        </slide>
+      </carousel>
+      
+      <div id="movie_card">
 
-      <Movies
-        :movies="toprated_movies"
-        @click.native="newAlert"
-        group_title="TOP RANKED"
-        class="mt-5"
-      />
-
-      <Movies
+      <h2>POPULAR</h2>
+      <MovieCard
         :movies="popular_movies"
-        @click.native="newAlert"
         group_title="POPULAR"
         class="mt-5"
       />
 
-      <Movies
+      <h2>TOP RATED</h2>
+      <MovieCard
+        :movies="toprated_movies"
+        group_title="TOP RANKED"
+        class="mt-5"
+      />
+
+      <!-- <MovieCard
         :isLogin="isLogin"
-        :movies="nowplaying_movies"
+        :movies="recommend_movie_user"
         @click.native="newAlert"
         group_title="RECOMMENDATION"
         class="mt-5"
-      />
+      /> -->
 
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+require('dotenv').config()
+import { Carousel, Slide } from "vue-carousel"
 import MovieCard from '@/components/MovieCard'
-const MOVIE_DB_API_URL_POPULAR = 'https://api.themoviedb.org/3/movie/popular'
-const MOVIE_DB_API_URL_TOP_RATED = 'https://api.themoviedb.org/3/movie/top_rated'
-const MOVIE_DB_API_KEY = 'd96fad9400bff08e7653e874066ac99b'
+import axios from 'axios'
+
+const TMDB_API_URL_POPULAR = 'https://api.themoviedb.org/3/movie/popular'
+const TMDB_API_URL_TOP_RATED = 'https://api.themoviedb.org/3/movie/top_rated'
+const TMDB_API_KEY = process.env.VUE_APP_TMDB_API_KEY
+
 export default {
   name: 'Movie',
   data: function () {
     return {
       popular_movies: [],
       toprated_movies: [],
+      recommend_movie_user: [],
+      movie_detail: []
     }
   },
   props: {
@@ -51,11 +64,13 @@ export default {
   },
   components: {
     MovieCard,
+    Carousel,
+    Slide
   },
   created: function () {
-    axios.get(MOVIE_DB_API_URL_POPULAR, {
+    axios.get(TMDB_API_URL_POPULAR, {
       params: {
-        api_key: MOVIE_DB_API_KEY,
+        api_key: TMDB_API_KEY,
         language: "ko-KR",
         page: 1,
         region: 'kr'
@@ -63,11 +78,12 @@ export default {
     })
     .then((res) =>{
       this.popular_movies = res.data.results
+      console.log(res.data.results)
     })
     .catch(err => console.log(err))
-    axios.get(MOVIE_DB_API_URL_TOP_RATED, {
+    axios.get(TMDB_API_URL_TOP_RATED, {
       params: {
-        api_key: MOVIE_DB_API_KEY,
+        api_key: TMDB_API_KEY,
         language: "ko-KR",
         page: 1,
         region: 'kr'
@@ -77,23 +93,42 @@ export default {
       this.toprated_movies = res.data.results
     })
     .catch(err => console.log(err))
+
+    // const token = localStorage.getItem('jwt')
+    // const config = {
+    //   headers: {
+    //     Authorization: `JWT ${token}`
+    //   }
+    // }
+    // 영화추천
+    // axios.get('http://127.0.0.1:8000/movies/recommend_movie_user/', config)
+    //   .then((res) => {
+    //     console.log(res)
+    //     this.recommend_movie_user = res.data
+    //   })
+    //   .catch((err) => {
+    //     console.log(err)
+    //   })
   },
-  methods: {
-    newAlert: function () {
-      if (this.isLogin === false) {
-      this.$fire({
-        title: "로그인을 해주세요!",
-        text: "",
-        type: "error",
-      }).then(r => {
-        console.log(r.value);
-      });
-      }
-    },
-  }
+  // methods: {
+  //   newAlert: function () {
+  //     if (this.isLogin === false) {
+  //     this.$fire({
+  //       title: "로그인을 해주세요!",
+  //       text: "",
+  //       type: "error",
+  //     }).then(r => {
+  //       console.log(r.value);
+  //     });
+  //     }
+  //   },
+  // }
 }
 </script>
 
 <style>
+  .banner_img {
+    opacity: 0.77;
+  }
 
 </style>
