3adc416a4020f80342babb1edc01bd2b8e8ce8d0
rosieyeon <sseyeon956@gmail.com>
2021-11-24 22:06:19 +0900

    feat: footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="footer__icons d-flex justify-center p-2 mt-40">
+      <img src="../assets/icon/akar-icons_github-fill.png">
+      <img src="../assets/icon/akar-icons_instagram-fill.png">
+      <img src="../assets/icon/dashicons_facebook-alt.png">
+      <img src="../assets/icon/akar-icons_twitter-fill.png">
+      <img src="../assets/icon/akar-icons_pinterest-fill.png">
+    </div>
+    <hr>
+    <div class="footer__content">
+      <p class="p-10">© 2021 Seok Young and Seung Eun. All Rights Reserved</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .footer_icons {
+    justify-content: center;
+  }
+  img {
+    margin-left: 10px;
+    margin-right:10px;
+  }
+
+</style>
