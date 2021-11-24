d0417bf680bfbad9b168caff06cdf8f0f18b5ae9
rosieyeon <sseyeon956@gmail.com>
2021-11-24 16:46:15 +0900

    feat: search-bar

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
+import axios from 'axios'
 
 Vue.use(Vuex)
+const TMDB_API_URL_POPULAR = 'https://api.themoviedb.org/3/movie/popular'
+// const TMDB_API_URL_TOP_RATED = 'https://api.themoviedb.org/3/movie/top_rated'
+const TMDB_API_KEY = process.env.VUE_APP_TMDB_API_KEY
 
 export default new Vuex.Store({
   state: {
     reviews: [],
     username: '',
+    popular_movies: [],
+    movies_title: [],
     selectedMovie: 0
   },
   mutations: {
     getUsername (state, username) {
       state.username = username
+    },
+    getMovies (state, popular_movies) {
+      state.popular_movies = popular_movies
+    },
+    getMoviesTitle (state, popular_movies) {
+      popular_movies.forEach(function (movie) {
+        state.movies_title.push(movie.title)
+      });
+    },
+    removeMoviesTitle (state) {
+      state.movies_title = []
     },
     SELECT_MOVIE: function (state, payload) {
       state.selectedMovie = payload
@@ -21,11 +38,28 @@ export default new Vuex.Store({
   actions: {
     selectMovie: function ({commit}, movieId) {
       commit('SELECT_MOVIE', movieId)
+    },
+    getMoviesMove (context) {
+      axios.get(TMDB_API_URL_POPULAR, {
+        params: {
+          api_key: TMDB_API_KEY,
+          language: "ko-KR",
+          page: 1,
+          region: 'kr'
+        }
+      })
+      .then((res) =>{
+        context.commit('getMovies', res.data.results)
+        console.log(res.data.results)
+      })
+      .catch(err => console.log(err))
     }
   },
   modules: {
   },
   plugins: [
-    createPersistedState()
+    createPersistedState({
+      paths: ["username","popular_movies"],
+    })
   ],
 })
